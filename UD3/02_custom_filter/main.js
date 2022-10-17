6c61415a203d60b245964bc4cfd023993358d36c
Alexander Aguirre <keevinaguirre@gmail.com>
2022-10-17 17:40:54 +0200

    3/2

--- UD3/02_custom_filter/main.js
+++ UD3/02_custom_filter/main.js
@@ -1,6 +1,20 @@
 const assert = require("assert").strict;
 
-function select(array, condition) {}
+function select(array, condition) {
+  return array.filter(condition);
+}
+
+function pairs(value) {
+  return value % 2 === 0;
+}
+
+function gt15(value) {
+  return value > 15;
+}
+
+function lt10(value) {
+  return value < 10;
+}
 
 let values = [1, 2, 3, 5, 7, 13, 17, 23, 29];
 
@@ -12,3 +26,5 @@ assert.deepStrictEqual(select(values, gt15), [17, 23, 29]);
 
 // menores de 10
 assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7]);
+
+console.log("OK");
