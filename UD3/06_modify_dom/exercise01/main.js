8d71c6364bb1fcb80384e2fcb69e2a2ddbf1ee9d
Alexander Aguirre <keevinaguirre@gmail.com>
2022-10-26 20:48:30 +0200

    Entregable

--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -2,17 +2,32 @@ window.onload = function (e) {
   console.log("documento cargado");
 
   // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
-  /**
-   * Cambiar el título del segundo h2
-   * Seleccionar el elemento con id == username
-   * Cambiar el color de todos los .first que estén dentro de un artículo
-   * Seleccionar todos lo elementos li con class == item
-   * Seleccionar todos lo buttons dentro de class == buttons
-   * Cambiar el código de fondo del primer párrafo
-   * Cambiar el color de frente (forecolor) de los elementos buttons dentro de class == buttons
-   */
-  document.querySelectorAll("h2")[1].innerHTML("TEST");
-  document.getElementById("username");
-  document.querySelectorAll("li .item");
-  document.querySelectorAll("button .buttons");
+
+  /* * Cambiar el título del segundo h2 */
+  let h2s = document.getElementsByTagName("h2");
+  h2s[1].innerHTML = "New text for the second h2";
+
+  /* * Seleccionar el elemento con id == username */
+  let username = document.getElementById("username");
+
+  /* * Cambiar el color de todos los .first que estén dentro de un artículo */
+  let firsts = document.querySelectorAll("article .first");
+  for (const element of firsts) {
+    element.style.color = "blue";
+  }
+
+  /* * Seleccionar todos lo elementos li con class == item */
+  let items = document.querySelectorAll("li.item");
+
+  /* * Seleccionar todos lo buttons dentro de class == buttons */
+  let buttons = document.querySelectorAll(".buttons button");
+
+  /* Cambiar el código de fondo del primer párrafo */
+  let firstParagraph = document.querySelector("p");
+  firstParagraph.style.backgroundColor = "red";
+
+  /* Cambiar el color de frente (forecolor) de los elementos buttons dentro de class == buttons */
+  for (const button of buttons) {
+    button.style.color = "green";
+  }
 };
